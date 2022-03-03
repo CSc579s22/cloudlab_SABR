06c7b5c07a3e69e2e5ad4415c6ea49a3e67b0d54
clarkzjw <i@jinwei.me>
2022-03-03 15:10:22 -0800

    migrate python2

--- server/http_capture.py
+++ server/http_capture.py
@@ -40,7 +40,7 @@ def GET_print(packet):
     try:
         client = pymongo.MongoClient()
         print("Connected successfully again!!!")
-    except pymongo.errors.ConnectionFailure, e:
+    except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB sadly: %s" % e)
     db = client.cachestatus
     table = db.cache1
@@ -49,7 +49,7 @@ def GET_print(packet):
     try:
         client = pymongo.MongoClient()
         print("Connected successfully again!!!")
-    except pymongo.errors.ConnectionFailure, e:
+    except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB sadly: %s" % e)
     db2 = client.cachestatus
     table2 = db2.cache1
@@ -57,7 +57,7 @@ def GET_print(packet):
     get_mpd = mpdinfo2.find_one({"urn": (str(f_path))})
     if get_mpd is not None:
         hit_count[str(get_mpd['quality'])] += 1
-    print hit_count
+    print(hit_count)
     # for res in table.find({"urn": str(http_packet["Path"])}).limit(1):
     if "init" not in f_path and "mpd" not in f_path:
         if res is None:
@@ -72,21 +72,21 @@ def cache_hit(res):
     try:
         client = pymongo.MongoClient()
         print("Connected successfully again!!!")
-    except pymongo.errors.ConnectionFailure, e:
+    except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB sadly: %s" % e)
     db = client.cachestatus
     table = db.cache1
     curr_hit = int(res['hit_rate'])
     res2 = table.find_one({'$query': {}, '$orderby': {"date": -1}})
     if res2 is not None:
-        print "**********HIT \t Get cache_size\n"
-        # print res2['cache_size']
-        # print res2['qual_no']
+        print("**********HIT \t Get cache_size\n")
+        # print(res2['cache_size'])
+        # print(res2['qual_no'])
 
     up_date = table.update_one({'urn': res['urn']}, {'$inc': {"hit_rate": 1}})
     up_date = table.update_one({'urn': res['urn']},
                                {'$set': {'date': datetime.utcnow(), 'cache_size': res2['cache_size']}})
-    # print "Cache hit\n"
+    # print("Cache hit\n")
 
 
 sniff(iface='eth1', prn=http_header, store=0, lfilter=lambda p: "GET" in str(p), filter="tcp[32:4] = 0x47455420")
